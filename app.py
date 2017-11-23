dff768ce652f59e93052f8b79a7708066ed76559
Andrew James <ajames20@gmail.com>
2017-11-23 10:59:24 +0200

    update fb url

--- app.py
+++ app.py
@@ -62,10 +62,7 @@ def fbconnect():
         'web']['app_id']
     app_secret = json.loads(
         open('/var/www/FlaskApp/FlaskApp/fb_client_secrets.json', 'r').read())['web']['app_secret']
-    url = 'https://graph.facebook.com/oauth/access_token?'\
-          'grant_type=fb_exchange_token&client_id=%s&'\
-          'client_secret=%s&fb_exchange_token=%s' % (
-              app_id, app_secret, access_token)
+    url = ('https://graph.facebook.com/v2.11/oauth/access_token?grant_type=fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s') % (app_id, app_secret, access_token)
     h = httplib2.Http()
     result = h.request(url, 'GET')[1]
 
