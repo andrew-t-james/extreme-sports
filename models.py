1be3f54448c25ddc532f8db1579e41558b09497c
Andrew James <ajames20@gmail.com>
2017-11-23 06:41:28 +0200

    update sport model

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, TEXT
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -42,7 +42,7 @@ class Sports(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
     description = Column(String(1000))
-    description_link = Column(String())
+    description_link = Column(TEXT())
     image_link = Column(String(1000))
     category_id = Column(Integer, ForeignKey('categories.id'))
     category = relationship('Categories')
